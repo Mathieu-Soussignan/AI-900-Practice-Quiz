6ef14fde9e20caee1fcd0341176b9482b8b77ae6
Mathieu Soussignan <contact.mathieu.soussignan@gmail.com>
2026-03-10 16:09:00 +0100

    Fix -> import_questions.py

--- import_questions.py
+++ import_questions.py
@@ -2,7 +2,7 @@ import json
 from supabase import create_client
 
 SUPABASE_URL = "https://qsmryrxztauatobrtbww.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzbXJ5cnh6dGF1YXRvYnJ0Ynd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNjM3OTgsImV4cCI6MjA4ODYzOTc5OH0.ng_hCNpiy8t_9NjTHm_zj4JzjxsjMnX5AKg5EBmoB1I"
+SUPABASE_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
